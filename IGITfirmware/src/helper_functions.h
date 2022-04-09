a2ce87fb65c23849119301ee378700f80be0af37
Helena <leni.kern@gmail.com>
2022-04-09 14:45:39 +0200

    writing Flash only if changes are made

--- IGITfirmware/src/helper_functions.h
+++ IGITfirmware/src/helper_functions.h
@@ -75,12 +75,14 @@ void readButton() {
   if (igitBot.button(0, 1)) {
     gamestate.playing = true;
     setGamestate(&gamestate);
+    writeFlash();
     igitBot.led(0, 1, OFF);
     igitBot.led(0, 2, RED);
   }
   if (igitBot.button(0, 2)) {
     gamestate.playing = false;
     setGamestate(&gamestate);
+    writeFlash();
     igitBot.led(0, 1, GREEN);
     igitBot.led(0, 2, OFF);
   }
@@ -88,6 +90,7 @@ void readButton() {
     igitBot.setCompass();
     gamestate.head = head;
     setGamestate(&gamestate);
+    writeFlash();
   }
   if (igitBot.button(3, 2)) {
     Serial.printf("kicked.\n");
